7c0dd0e76cb8f1e12c474e51983021f591014a29
Djordje Perovic <djordje.perovic@amplitudo.me>
2026-02-10 16:08:07 +0100

    Adds reaction btns

--- composeApp/src/commonMain/kotlin/com/bokadev/word_takes/presentation/home/WordCard.kt
+++ composeApp/src/commonMain/kotlin/com/bokadev/word_takes/presentation/home/WordCard.kt
@@ -6,30 +6,36 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.dropShadow
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.shadow.Shadow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.bokadev.word_takes.core.utils.formatCreatedAt
 import com.bokadev.word_takes.domain.model.WordItem
 import llc.amplitudo.cerovo.ui.theme.WordTakesTheme
+import org.jetbrains.compose.resources.vectorResource
+import word_takes.composeapp.generated.resources.Res
+import word_takes.composeapp.generated.resources.ic_angry
+import word_takes.composeapp.generated.resources.ic_thumbs_down
+import word_takes.composeapp.generated.resources.ic_thumbs_up
+import word_takes.composeapp.generated.resources.party_popper
 
 @Composable
 fun WordCard(
     modifier: Modifier = Modifier,
     wordItem: WordItem
 ) {
+
 
     Column(
         verticalArrangement = Arrangement.spacedBy(12.dp, alignment = Alignment.Top),
@@ -69,6 +75,120 @@ fun WordCard(
                 color = WordTakesTheme.colors.wordTakesWhite,
                 style = WordTakesTheme.typogrpahy.geistSemiBold24,
                 letterSpacing = 20.sp
+            )
+        }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Button(
+                onClick = {
+                },
+                modifier = Modifier.wrapContentWidth()
+                    .height(45.dp)
+                    .clip(
+                        RoundedCornerShape(
+                            8.dp
+                        )
+                    ),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = WordTakesTheme.colors.wordTakesGood,
+                    contentColor = WordTakesTheme.colors.wordTakesWhite,
+                ),
+                elevation = ButtonDefaults.buttonElevation(
+                    defaultElevation = 6.dp
+                ),
+                content = {
+                    Icon(
+                        imageVector = vectorResource(Res.drawable.ic_thumbs_up),
+                        contentDescription = null,
+                        tint = WordTakesTheme.colors.backgroundSecondary
+                    )
+                }
+            )
+
+            Button(
+                onClick = {
+                },
+                modifier = Modifier.wrapContentWidth()
+                    .height(45.dp)
+                    .clip(
+                        RoundedCornerShape(
+                            8.dp
+                        )
+                    ),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = WordTakesTheme.colors.wordTakesAmazing,
+                    contentColor = WordTakesTheme.colors.wordTakesWhite,
+                ),
+                elevation = ButtonDefaults.buttonElevation(
+                    defaultElevation = 6.dp
+                ),
+                content = {
+                    Icon(
+                        imageVector = vectorResource(Res.drawable.party_popper),
+                        contentDescription = null,
+                        tint = WordTakesTheme.colors.backgroundSecondary
+                    )
+                }
+            )
+
+            Button(
+                onClick = {
+                },
+                modifier = Modifier.wrapContentWidth()
+                    .height(45.dp)
+                    .clip(
+                        RoundedCornerShape(
+                            8.dp
+                        )
+                    ),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = WordTakesTheme.colors.wordTakesBad,
+                    contentColor = WordTakesTheme.colors.wordTakesWhite,
+                ),
+                elevation = ButtonDefaults.buttonElevation(
+                    defaultElevation = 6.dp
+                ),
+                content = {
+                    Icon(
+                        imageVector = vectorResource(Res.drawable.ic_thumbs_down),
+                        contentDescription = null,
+                        tint = WordTakesTheme.colors.backgroundSecondary
+                    )
+                }
+            )
+
+            Button(
+                onClick = {
+                },
+                modifier = Modifier.wrapContentWidth()
+                    .height(45.dp)
+                    .clip(
+                        RoundedCornerShape(
+                            8.dp
+                        )
+                    ),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = WordTakesTheme.colors.wordTakesAwful,
+                    contentColor = WordTakesTheme.colors.wordTakesWhite,
+                ),
+                elevation = ButtonDefaults.buttonElevation(
+                    defaultElevation = 6.dp
+                ),
+                content = {
+                    Icon(
+                        imageVector = vectorResource(Res.drawable.ic_angry),
+                        contentDescription = null,
+                        tint = WordTakesTheme.colors.backgroundSecondary
+                    )
+                }
             )
         }
     }
